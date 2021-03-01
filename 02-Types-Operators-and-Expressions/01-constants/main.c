3f20a9bf5aa88d6e4c5bc5fb77c56883059ccb74
Misael1998 <misalande@gmail.com>
2021-03-01 15:56:57 -0600

    constants

---
+++ 02-Types-Operators-and-Expressions/01-constants/main.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+//Constants
+#define INT 1234          //int type constant
+#define LNG 123456789L;   //long type constant
+#define UNG 123UL;        //unsigned long type constant
+#define FLT 12.3          //float type constant
+#define HEX 0x1f          //int type constant in hexadecimal
+#define OCT 037           //int type constant in octal
+#define CCI 'x'           //int type constant of int value of x
+#define VTB '\013'        //ASCII vertical tab
+
+//enumeration constants
+enum boolean { NO, YES };
+enum escapes { BELL = '\a', BACKSPACE = '\b', TAB = '\t',
+               NEWLINE = '\n', VTAB = '\v', RETURN = '\r' };
+
+
+int main()
+{
+  return 0;
+}
