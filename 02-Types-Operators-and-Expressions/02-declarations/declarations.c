10be8409979e2d91da236bec6b071dc8ebea8580
Misael1998 <misalande@gmail.com>
2021-03-01 16:04:19 -0600

    declarations

---
+++ 02-Types-Operators-and-Expressions/02-declarations/declarations.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+#define  MAXLINE 1000
+
+int main()
+{
+  //list type declaration
+  int lower, upper, step;
+  //distributed type declaration
+  char c;
+  char line[1000];
+  
+  //variable inicialization in declaration
+  char  esc = '\\';
+  int   i = 0;
+  int   limit = MAXLINE+1;
+  float eps = 1.0e-5;
+
+  //cualifier const
+  const double e = 2.71828182845905;
+  const char msg[] = "warning: ";
+
+  return 0;
+}
