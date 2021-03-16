3049b964bcbed5f2a484dad93f347253631329e0
Misael1998 <misalande@gmail.com>
2021-03-16 07:05:51 -0600

    structures and functions

---
+++ 05-structures/01-structures-and-functions/addpoint.c
@@ -0,0 +1,9 @@
+#include "point.h"
+
+/* addpoint: add to points */
+struct point addpoint(struct point p1, struct point p2)
+{
+  p1.x += p2.x;
+  p1.y += p2.y;
+  return p1;
+}
---
+++ 05-structures/01-structures-and-functions/makepoint.c
@@ -0,0 +1,11 @@
+#include "point.h"
+
+/* makepoint: make a point from x and y components */
+struct point makepoint(int x, int y)
+{
+  struct point temp;
+
+  temp.x = x;
+  temp.y = y;
+  return temp;
+}
