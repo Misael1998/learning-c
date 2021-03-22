d87ffb298cdef2939825f3c4c7686d82d5b89d01
Misael1998 <misalande@gmail.com>
2021-03-22 16:02:19 -0600

    typedef

---
+++ 05-structures/06-typedef/treenode.h
@@ -0,0 +1,19 @@
+#ifndef treenode
+#define treenode
+
+#include <stdlib.h>
+
+typedef struct tnode *Treeptr;
+
+typedef struct tnode {
+  char *word;
+  int count;
+  Treeptr left;
+  Treeptr right;
+} Treenode;
+
+Treeptr talloc(void) 
+{
+  return (Treeptr) malloc(sizeof(Treenode));
+}
+#endif
